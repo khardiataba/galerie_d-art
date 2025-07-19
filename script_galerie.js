7f86e5f52b2a47fd5384e5d772e7fe06e0723663
khardiataba <khardiataba163@gmail.com>
2025-07-19 20:01:10 +0000

    Add files via upload

---
+++ script_galerie.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.getElementById('themeToggle');
+const body = document.body;
+
+toggleBtn.addEventListener('click', () => {
+  body.classList.toggle('dark');
+  toggleBtn.textContent = body.classList.contains('dark') ? '☀️ Mode clair' : '🌙 Mode sombre';
+});
+
